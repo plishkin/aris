851b6ef34ab5bcf8e0cbd9c221ab7a3315ee4beb
Viacheslav Plishkin <viacheslav.plishkin@gmail.com>
2024-04-24 12:05:02 +0300

    require default admin

--- mysite/code/extensions/MemberExtension.php
+++ mysite/code/extensions/MemberExtension.php
@@ -19,10 +19,24 @@ class MemberExtension extends \DataExtension
 	public function requireDefaultRecords()
 	{
 		parent::requireDefaultRecords();
-		$admin = \Security::findAnAdministrator();
-		if (!$admin->Password) {
-			$admin->Password = $_ENV['SS_DEFAULT_ADMIN_PASSWORD'];
-			$admin->write();
+
+		$adminLogin = $_ENV['SS_DEFAULT_ADMIN_USERNAME'];
+		$adminPass = $_ENV['SS_DEFAULT_ADMIN_PASSWORD'];
+		if ($adminLogin && $adminPass) {
+			$admin = \Member::get()->filter('Email', $adminLogin)->first();
+			if (!$admin) {
+				$admin = \Member::create();
+				$admin->Email = $adminLogin;
+				$admin->FirstName = _t('Member.DefaultAdminFirstname', 'Default Admin');
+				$admin->write();
+				$admin->Password = $adminPass;
+				$admin->write();
+				// find a group with ADMIN permission
+				$adminGroup = \Permission::get_groups_by_permission('ADMIN')->First();
+				$adminGroup
+					->DirectMembers()
+					->add($admin);
+			}
 		}
 	}
 
